301058cb2aba1849d9de21f015e3672dcf03d241
Artemiy111 <mrart111>
2023-01-20 23:14:12 +0300

    fix: ui

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@ watchEffect(() => {
 <template>
   <div class="container mx-auto mt-10 mb-10 px-5 transition-all">
     <TheHeader class="" @toggle-darkmode="toggleDark" />
-    <TheMain class="mt-10 flex flex-col flex-nowrap gap-x-20 gap-y-10 md:flex-row" />
+    <TheMain class="mt-10" />
   </div>
 </template>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -9,8 +9,8 @@ const dataset = computed(
 </script>
 
 <template>
-  <main>
-    <InputData v-model="inputData" class="md:min-w-[11rem]" />
-    <ErrorCalculation :dataset="dataset" />
+  <main class="flex flex-col flex-nowrap justify-between gap-x-20 gap-y-10 md:flex-row">
+    <InputData v-model="inputData" class="md:max-w-[12rem]" />
+    <ErrorCalculation :dataset="dataset" class="" />
   </main>
 </template>
