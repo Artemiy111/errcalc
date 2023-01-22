365f24ac60f36dc5cebb019c48794d6d2a009450
Artemiy111 <mrart111@mail.ru>
2023-01-23 00:19:59 +0300

    fix: test coverage configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
+import type { UserConfig as VitestConfig } from 'vitest'
 
 import vue from '@vitejs/plugin-vue'
 
@@ -8,13 +9,14 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
+  test: { globals: true, environment: 'jsdom' } as VitestConfig,
   plugins: [
     vue(),
     AutoImport({
       dts: true,
       imports: ['vue'],
       eslintrc: {
-        enabled: true, // <-- this
+        enabled: true,
       },
     }),
     Components({ dts: true }),
